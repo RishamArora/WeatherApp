da8e8d55f6af4be61b5711944d410467ef6b26d7
RishamArora <69429468+RishamArora@users.noreply.github.com>
2024-03-30 12:27:03 +0530

    My WeatherApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule                      
   ],
   providers: [
     provideClientHydration()
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com',
+XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+XRapidAPIKeyHeaderValue: '622496f753msh533f0f9af30392ap1837cajsncaa689a1de90'
+};
